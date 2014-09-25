117b7622e81f4f0f9cd01acf7fe6c059e85a4c20
Stoyanov <stoyanov.vn@gmail.com>
2014-09-25 21:36:33 +0300

    Homework 3 - Exceptions Handling - completed

---
+++ SoftUni-Level2/Object-OrientedProgramming/Homeworks/3.ExceptionsHandling/1.SquareRoot/SquareRoot.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class SquareRoot
+{
+    static void Main()
+    {
+        try
+        {
+            Console.Write("Enter your number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.WriteLine("Square root: " + Math.Sqrt(number));
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Invalid number", ex);
+        }
+        finally
+        {
+            Console.WriteLine("Good bye");
+        }
+    }
+}
---
+++ SoftUni-Level2/Object-OrientedProgramming/Homeworks/3.ExceptionsHandling/2.EnterNubmers/EnterNumbers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class EnterNumbers
+{
+    public static int ReadNumber(int start, int end)
+    {
+        string numAsStr = Console.ReadLine();
+        int number = int.Parse(numAsStr);
+        if (number < start || number > end)
+        {
+            throw new FormatException("The number must be in the range: [" + start + " .. " + end + "]");
+        }
+        return number;
+    }
+
+    static void Main()
+    {
+        int counter = 0;
+        int start = 1;
+
+        while (counter < 10)
+        {
+            try
+            {
+                int currentNum = ReadNumber(start, 100);
+
+                if (currentNum > start)
+                {
+                    start = currentNum;
+                }
+                counter++;
+            }
+            catch (ArgumentException argex)
+            {
+                Console.WriteLine("{0} Repeat input!", argex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} Repeat input!", ex.Message);
+            }
+        }
+    }
+}
