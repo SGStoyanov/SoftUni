5e7dab652c758946a27cc7ed76acb18932b3e608
Stoyanov <stoyanov.vn@gmail.com>
2014-09-27 12:06:18 +0300

    Homework 4 - Other Types, Problems 1 and 2 - solved

---
+++ SoftUni-Level2/Object-OrientedProgramming/Homeworks/4.OtherTypes/1.GalacticGPS/GalacticGPS.cs
@@ -0,0 +1,70 @@
+﻿/* Problem 1.	Galactic GPS
+Create a struct Location that holds fields of type double latitude and longitude of a given location. Create an enumeration Planet that holds the following constants: Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune.
+•	Add an enum field of type Planet. Encapsulate all fields. 
+•	Add a constructor that holds 3 parameters: latitude, longitude and planet.
+•	Override ToString() to print the current location in the format <latitude>, <longitude> - <location> */
+
+using System;
+
+namespace OtherTypes
+{
+
+    // Enum
+    public enum Planet
+    {
+        Mercury, Venus, Earth, Mars, Jupiter, Saturn, Uranus, Neptune
+    }
+
+    struct Location
+    {
+        // Fields
+        private double latitude;
+        private double longitude;
+        private Planet planet;
+
+        // Properties
+        public double Latitude
+        {
+            get { return this.latitude; }
+            set { this.latitude = value; }
+        }
+
+        public double Longitude
+        {
+            get { return this.longitude; }
+            set { this.longitude = value; }
+        }
+
+        public Planet Planet
+        {
+            get { return this.planet; }
+            set { this.planet = value; }
+        }
+
+        // Constructor
+        public Location(double latitude, double longitude, Planet planet) : this()
+        {
+            this.Latitude = latitude;
+            this.Longitude = longitude;
+            this.planet = planet;
+        }
+    
+        // ToString() Method
+        public override string ToString()
+        {
+            return string.Format("{0}, {1} - {2}", this.latitude, this.longitude, this.planet);
+        }
+    }  
+
+    class GalacticGPS
+    {
+        static void Main()
+        {
+            Location home = new Location(18.037986, 28.870097, Planet.Earth);
+            Location loc2 = new Location(2.4939, 4843.39292196, Planet.Mars);
+
+            Console.WriteLine(home);
+            Console.WriteLine(loc2);
+        }
+    }
+}
---
+++ SoftUni-Level2/Object-OrientedProgramming/Homeworks/4.OtherTypes/2.FractionCalculator/FractionCalculator.cs
@@ -0,0 +1,90 @@
+﻿/* Problem 2.	Fraction Calculator
+Create a struct Fraction that holds the numerator and denominator of a fraction as fields. A fraction is the division of two rational numbers (e.g. 22 / 7, where 22 is the numerator and 7 is the denominator).
+•	The struct constructor takes the numerator and denominator as arguments. They are integer numbers in the range [-9223372036854775808…9223372036854775807]. 
+•	Validate the input through properties. The denominator cannot be 0. Throw proper exceptions with descriptive messages.
+•	Overload the + and - operators to perform addition and subtraction on objects of type Fraction. The result should be a new Fraction.
+•	Override ToString() to print the fraction in floating-point format. */
+
+using System;
+
+namespace OtherTypes
+{
+
+    struct Fraction
+    {
+        private long numerator;
+        private long denominator;
+
+        public long Numerator
+        {
+            get { return this.numerator; }
+            set
+            {
+                try
+                {
+                    this.numerator = value;
+                }
+                catch (ArgumentException argex)
+                {
+                    Console.WriteLine("Bad numerator\n" + argex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Wrong data\n" + ex.Message);
+                }
+            }
+        }
+
+        public long Denominator
+        {
+            get { return this.denominator; }
+            set
+            {
+                if (value == 0)
+                {
+                    throw new ArgumentException("The denominator couldn't be zero!");
+                }
+                this.denominator = value;
+            }
+        }
+
+        // Main contstructor
+        public Fraction(long num, long denum) : this()
+        {
+            this.Numerator = num;
+            this.Denominator = denum;
+        }
+
+        public static Fraction operator +(Fraction frac1, Fraction frac2)
+        {
+            long num = frac1.numerator * frac2.denominator + frac2.numerator * frac1.denominator;
+            long denum = frac1.denominator * frac2.denominator;
+            return new Fraction(num, denum);
+        }
+        public static Fraction operator -(Fraction frac1, Fraction frac2)
+        {
+            long num = frac1.numerator * frac2.denominator - frac2.numerator * frac1.denominator;
+            long denum = frac1.denominator * frac2.denominator;
+            return new Fraction(num, denum);
+        }
+
+        public override string ToString()
+        {
+            double result = (double)this.numerator/(double)this.denominator;
+            return result.ToString();
+        }
+    }
+
+    class FractionCalculator
+    {
+        static void Main()
+        {
+            Fraction fraction1 = new Fraction(22, 7);
+            Fraction fraction2 = new Fraction(40, 4);
+            Fraction result = fraction1 + fraction2;
+            Console.WriteLine(result.Numerator);
+            Console.WriteLine(result.Denominator);
+            Console.WriteLine(result);
+        }
+    }
+}
