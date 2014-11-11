ecc7787e91344126af4fb74d3416e69cc43804bf
SGStoyanov <stoyanov.vn@gmail.com>
2014-11-11 11:33:58 +0200

    Homeworks 3 and 4 - Modules and Patterns and AMD and RequireJS - done

---
+++ SoftUni-Level2/JavaScriptOOP/Homeworks/4.AMDandRequireJS/scripts/main.js
@@ -0,0 +1,17 @@
+﻿(function () {
+
+    require.config({
+        paths: {
+            'container': 'container',
+            'section': 'section',
+            'item': 'item'
+        }
+    });
+
+    require(['container'], function(Container) {
+        var conte = new Container('Title');
+        console.log(conte);
+        conte.addToDOM(document.body);
+    });
+
+})();
