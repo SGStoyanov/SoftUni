c95fda2fb6c37da71a798a4fa9cc290941e29a9d
SGStoyanov <stoyanov.vn@gmail.com>
2014-11-30 14:37:21 +0200

    Updated Parse.com problem

--- SoftUni-Level2/JavaScriptApps/Homeworks/3.BaaS-and-Parse.com/ListCountries/ParseComManipulator.js
+++ SoftUni-Level2/JavaScriptApps/Homeworks/3.BaaS-and-Parse.com/ListCountries/ParseComManipulator.js
@@ -60,22 +60,62 @@
     //});
 
     // Retrieving data
-    $.ajax({
-        method: "GET",
-        headers: {
-            "X-Parse-Application-Id": PARSE_APP_ID,
-            "X-Parse-REST-API-Key": PARSE_REST_API_KEY
-        },
-        url: "https://api.parse.com/1/classes/Country"
-    }).success(function (data) {
+    loadCountries();
+    function loadCountries() {
+        $.ajax({
+            method: "GET",
+            headers: {
+                "X-Parse-Application-Id": PARSE_APP_ID,
+                "X-Parse-REST-API-Key": PARSE_REST_API_KEY
+            },
+            url: "https://api.parse.com/1/classes/Country",
+            success: countriesLoaded,
+            error: ajaxError
+        });
+    }
+    
+    function countriesLoaded(data) {
         for (var c in data.results) {
             var country = data.results[c];
-            //console.log(country);
             var countryItem = $('<li>').css('list-style-type', 'square');
-            countryItem.text(country.name);
+            var countryLink = $("<a href='#'>").text(country.name);
+            $(countryLink).data('country', country);
+            countryLink.appendTo(countryItem);
+            $(countryLink).click(countryClicked);
             countryItem.appendTo($("#countriesList"));
         }
-    }).error(function () {
-        alert('Cannot load towns.');
-    });
+    }
+
+    function countryClicked() {
+        var country = $(this).data('country');
+        $('#towns').hide();
+        $('#towns h2').text(country.name);
+        var countryId = country.objectId;
+        $.ajax({
+            method: "GET",
+            headers: {
+                "X-Parse-Application-Id": PARSE_APP_ID,
+                "X-Parse-REST-API-Key": PARSE_REST_API_KEY
+            },
+            url: 'https://api.parse.com/1/classes/Town?where={"country":{"__type":"Pointer","className":"Country","objectId":"' + countryId + '"}}',
+            success: townsLoaded,
+            error: ajaxError
+        });
+    }
+
+    function townsLoaded(data) {
+        $('#towns ul').html('');
+        for (var t in data.results) {
+            var town = data.results[t];
+            var townItem = $('<li>');
+            townItem.text(town.name); // ...
+            townItem.appendTo($('#towns ul'));
+        }
+        $('#towns').show();
+    }
+
+    function ajaxError() {
+        console.log('Cannot load AJAX data');
+    }
+
 });
