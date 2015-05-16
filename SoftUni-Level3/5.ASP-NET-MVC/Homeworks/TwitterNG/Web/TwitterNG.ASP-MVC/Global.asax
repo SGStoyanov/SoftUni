c499f298c9c6026ebfd6322fcd96e71d03dfbdfb
SGStoyanov <stoyanov.vn@gmail.com>
2015-05-16 14:38:50 +0300

    ASP.NET MVC 5 - TwitterNG initial commit

---
+++ SoftUni-Level3/5.ASP-NET-MVC/Homeworks/TwitterNG/Web/TwitterNG.ASP-MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TwitterNG.ASP_MVC.MvcApplication" Language="C#" %>
---
+++ SoftUni-Level3/5.ASP-NET-MVC/Homeworks/TwitterNG/Web/TwitterNG.ASP-MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwitterNG.ASP_MVC.Startup))]
+namespace TwitterNG.ASP_MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
